f87f713a764db595debff05ea6804a03a4552a82
Jeff West <jeff10278@me.com>
2025-08-10 10:32:32 -0500

    update version 3.1.3

--- docs/conf.py
+++ docs/conf.py
@@ -23,8 +23,8 @@ sys.path.insert(0, str(src_dir))
 project = "project-x-py"
 copyright = "2025, Jeff West"
 author = "Jeff West"
-release = "3.1.2"
-version = "3.1.2"
+release = "3.1.3"
+version = "3.1.3"
 
 # -- General configuration ---------------------------------------------------
 
